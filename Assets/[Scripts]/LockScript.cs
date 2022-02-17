415e46133408827c39be4cae8e1a0ddf0a67cde6
Robert Palermo <robert.palermo.official@gmail.com>
2022-02-16 20:23:01 -0500

    Added Lock Script

---
+++ Assets/[Scripts]/LockScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockScript : MonoBehaviour
+{
+    [SerializeField, Range(0.0f, 1.0f)]
+    public float lockDifficulty = 0.5f;
+
+    [SerializeField, Range(30.0f, 350.0f)]
+    public float lockRange = 180.0f;
+
+    public float lockAngle { get; private set; }
+
+    private void Awake()
+    {
+        GetNewLockAngle();
+    }
+
+    /// Functions ///
+
+    public void GetNewLockAngle()
+    {
+        lockAngle = Random.Range(10.0f, lockRange - 10.0f);
+    }
+}
